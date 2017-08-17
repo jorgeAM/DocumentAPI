dfd7050cfe5f345fb0d16e01456aead6c7b4c505
Jorge L. Alfaro <jorge.alfmur@gmail.com>
2017-08-16 22:05:29 -0500

    creamos datos ficticios para usar en la API

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |i|
+	MyDocument.create(
+	title: "Documento #{i}",
+	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed libero sit amet nibh vehicula venenatis. Nam id nulla faucibus, porttitor sapien ac, ullamcorper dolor. Vestibulum in ante leo. Pellentesque ligula nisl, interdum et nulla vel, efficitur pulvinar lacus. Etiam venenatis in nisi non rhoncus. Vivamus tincidunt, orci tempus consequat congue, risus lorem semper nisl, porttitor pulvinar tortor enim vitae odio. Donec nec sagittis augue. Integer tincidunt dapibus nibh a efficitur. Nunc porta tempus ipsum non pellentesque. Ut malesuada quam nec nulla egestas pretium.',
+	file_url: 'https://www.youtube.com/watch?v=OQ6ITgM7I1A&feature=share',
+	image_url: 'https://scontent.flim5-3.fna.fbcdn.net/v/t1.0-9/1520741_241875559308031_92402000_n.jpg?oh=fb1260c19712726d584febfb4bfa9cf4&oe=5A37366D'
+	)	
+end
